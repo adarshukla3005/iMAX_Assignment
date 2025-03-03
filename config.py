c47dcbc2a7d80f464656d7622a64432c372556a6
Adarsh Shukla <122630902+adarshukla3005@users.noreply.github.com>
2025-03-03 06:45:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 from langchain_google_genai import GoogleGenerativeAI
 
-llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key="AIzaSyB7UrX_kgBj6OrNBGfSfaDMhNC_wIVBkCA")
+llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key="your_gemini_api_key")
